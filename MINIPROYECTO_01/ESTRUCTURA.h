fdfa9102f612234b7ed8a39b1a45f7e1f23d6990
Alejandra Romero Aro <alejandra.romero.a@ucb.edu.bo>
2026-06-09 23:30:58 -0400

    Add files via upload

---
+++ MINIPROYECTO_01/ESTRUCTURA.h
@@ -0,0 +1,22 @@
+#ifndef ESTRUCTURA_H_INCLUDED
+#define ESTRUCTURA_H_INCLUDED
+
+struct structEstudiante
+{
+    char ci[10];
+    char nombres[30];
+    char apellidos[30];
+};
+
+struct structNotas
+{
+    char ci[10];
+    char materia[30];
+    int nota;
+};
+
+#endif
+
+
+
+#endif // ESTRUCTURA_H_INCLUDED
